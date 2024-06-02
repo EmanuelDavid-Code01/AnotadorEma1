1f3a4959b1523cbf863838e514345d6753a8a3ad
EmanuelDavid <emadg07.ed@gmail.com>
2024-06-02 18:46:01 -0300

    Añadir componentes TodoItem y TodoList, y modificar App.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList({ todos, addTodo, deleteTodo }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      addTodo(newTodo);
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Agregar una tarea"
+      />
+      <button onClick={handleAddTodo}>Agregar</button>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
